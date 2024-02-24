1647822c6f82842d40992b939b9b9707e7acb497
Alyssa Kucharyski <adkucharyski@gmail.com>
2024-02-24 14:08:24 -0700

    color change

--- retryTutorial/src/js/main.js
+++ retryTutorial/src/js/main.js
@@ -8,7 +8,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 // Create a cube: we need a geometry to define the shape of the object, and a material to define the appearance of the object.
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x21f30 });
+const material = new THREE.MeshBasicMaterial({ color: 0x51a60 });
 const cube = new THREE.Mesh( geometry, material );
 
 // Add the cube to the scene
