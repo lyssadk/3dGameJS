fe34b6a28d21446f1d814c9e1838c2d5910e0a22
Alyssa Kucharyski <adkucharyski@gmail.com>
2024-02-24 16:49:28 -0700

    messing with key logic

--- retryTutorial/src/js/main.js
+++ retryTutorial/src/js/main.js
@@ -1,26 +1,93 @@
 import * as THREE from 'three';
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+// code copied from https://threejs.org/manual/#en/fundamentals with minor modifications
+function main() {
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-// Create a cube: we need a geometry to define the shape of the object, and a material to define the appearance of the object.
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x51a60 });
-const cube = new THREE.Mesh( geometry, material );
+	const canvas = document.querySelector( '#c' );
+	const renderer = new THREE.WebGLRenderer( { antialias: true, canvas, alpha: true,  premultipliedAlpha: false } );
 
-// Add the cube to the scene
-scene.add( cube );
-camera.position.z = 5;
+	const fov = 75;
+	const aspect = 2; // the canvas default
+	const near = 0.1;
+	const far = 5;
+	const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+	camera.position.z = 2;
 
-// render the scene using animationFrame so when the user moves to a different tab, the animation stops (saving processing power) vs using setInterval
-// and when the user comes back to the tab, the animation starts again.
-function animate() {
-	requestAnimationFrame( animate );
-    cube.rotation.x += 0.11;
-    cube.rotation.y += 0.01;
-	renderer.render( scene, camera );
+	const scene = new THREE.Scene();
+
+	{
+
+		const color = 0xFFFFFF;
+		const intensity = 3;
+		const light = new THREE.DirectionalLight( color, intensity );
+		light.position.set( - 1, 2, 4 );
+		scene.add( light );
+
+	}
+
+	const boxWidth = 1;
+	const boxHeight = 1;
+	const boxDepth = 1;
+	const geometry = new THREE.BoxGeometry( boxWidth, boxHeight, boxDepth );
+
+	function makeInstance( geometry, color, x ) {
+
+		const material = new THREE.MeshPhongMaterial( { color } );
+
+		const cube = new THREE.Mesh( geometry, material );
+		scene.add( cube );
+
+		cube.position.x = x;
+
+		return cube;
+
+	}
+    canvas.addEventListener('keydown', (e) => {
+        if (e.key === 'ArrowUp') {
+            camera.position.y += 7.1;
+        }
+        else if (e.key === 'ArrowDown') {
+            camera.position.y -= 5.1;
+            
+        }
+        else if (e.key === 'ArrowLeft') {
+            camera.position.x -= 5.1;
+        }
+        else if (e.key === 'ArrowRight') {
+            camera.position.x += 5.1;
+        }
+        else{
+            camera.position.z += 5.1;
+        }
+    });
+
+	const cubes = [
+		makeInstance( geometry, 0x44aa88, 0 ),
+		makeInstance( geometry, 0x8844aa, - 2 ),
+		makeInstance( geometry, 0xaa8844, 2 ),
+	];
+
+	function render( time ) {
+
+		time *= 0.001; // convert time to seconds
+
+		cubes.forEach( ( cube, ndx ) => {
+
+			const speed = 1 + ndx * .1;
+			const rot = time * speed;
+			cube.rotation.x = rot;
+			cube.rotation.y = rot;
+
+		} );
+
+		renderer.render( scene, camera );
+
+		requestAnimationFrame( render );
+
+	}
+
+	requestAnimationFrame( render );
+
 }
-animate();
+
+main();
